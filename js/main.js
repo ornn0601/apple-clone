1d8eb7f02df55e8ad5767c7a8bad82f176e66a8a
ornn0601 <info@ornn.co.kr>
2020-09-27 16:04:01 +0900

    apple clon code study

---
+++ js/main.js
@@ -0,0 +1,65 @@
+/* 모든 애니메이션의 정보의 배열을 제작 */
+
+/* 즉시 실행 함수
+- 지역변수로 작업하여 중복을 피한다.
+(() => {
+})();
+
+function () {
+}) ();
+*/
+
+(() => {
+
+    const sceneInfo = [
+        {
+            // 0
+            type: 'sticky',
+            heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector('#scroll-section-0')
+            }
+        },
+        {
+            // 1
+            type: 'normal',
+            heightNum: 5,
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector('#scroll-section-1')
+            }
+        },
+        {
+            // 2
+            type: 'sticky',
+            heightNum: 5,
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector('#scroll-section-2')
+            }
+        },
+        {
+            // 3
+            type: 'sticky',
+            heightNum: 5,
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector('#scroll-section-3')
+            }
+        }
+    ];
+
+    function setLayout() {
+        // 각 스크롤 섹션의 높이 세팅
+        for (let i = 0; i < sceneInfo.length; i++) {
+            sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+            sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+        }
+        console.log(sceneInfo);
+    }
+
+    window.addEventListener('resize', setLayout);
+    setLayout();
+
+})();
